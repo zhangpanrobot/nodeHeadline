204b7b24f4b42cda8af05dd531a275327aeda86b
zp <zhangpanrobot@gmail.com>
2015-05-12 14:57:04 +0800

    add

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('*', function(req, res) {
-	res.sendFile('public/index.html');
+	res.sendfile('public/index.html');
 });
 
 app.listen(3000);
